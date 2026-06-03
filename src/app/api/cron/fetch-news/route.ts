c52b9d6bec067d1e255000681aadb3f3ad3371b4
lucas112600 <lucas112600@users.noreply.github.com>
2026-06-03 21:28:15 +0800

    feat: add 50 game communities to official feeds

--- src/app/api/cron/fetch-news/route.ts
+++ src/app/api/cron/fetch-news/route.ts
@@ -5,65 +5,7 @@ import { ANONYMOUS_OWL } from '@/lib/db';
 
 const parser = new Parser();
 
-// 預設的官方 RSS 資訊來源
-const OFFICIAL_FEEDS = [
-  {
-    name: '巴哈姆特遊戲情報',
-    category: '遊戲',
-    url: 'https://gnn.gamer.com.tw/rss.xml',
-    logo_url: 'https://i.imgur.com/G5Z5oQ9.png',
-    description: '自動同步巴哈姆特 GNN 新聞，提供最新的遊戲、動漫情報。',
-  },
-  {
-    name: 'Minecraft 麥塊社群',
-    category: '遊戲',
-    url: 'https://news.google.com/rss/search?q=Minecraft+OR+當個創世神&hl=zh-TW&gl=TW&ceid=TW:zh-Hant',
-    logo_url: 'https://upload.wikimedia.org/wikipedia/en/5/51/Minecraft_cover.png',
-    description: '全球最受歡迎的沙盒遊戲，方塊世界的新鮮事。',
-  },
-  {
-    name: 'League of Legends 英雄聯盟',
-    category: '遊戲',
-    url: 'https://news.google.com/rss/search?q=英雄聯盟+OR+League+of+Legends&hl=zh-TW&gl=TW&ceid=TW:zh-Hant',
-    logo_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d8/League_of_Legends_2019_vector.svg',
-    description: 'LOL 官方更新、電競賽事與英雄調整情報。',
-  },
-  {
-    name: 'Apex Legends 英雄',
-    category: '遊戲',
-    url: 'https://news.google.com/rss/search?q=Apex+Legends+OR+Apex英雄&hl=zh-TW&gl=TW&ceid=TW:zh-Hant',
-    logo_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d0/Apex_legends_cover.jpg',
-    description: '捍衛者集合！Apex 英雄最新賽季與活動。',
-  },
-  {
-    name: '原神 Genshin Impact',
-    category: '遊戲',
-    url: 'https://news.google.com/rss/search?q=原神+OR+Genshin+Impact&hl=zh-TW&gl=TW&ceid=TW:zh-Hant',
-    logo_url: 'https://upload.wikimedia.org/wikipedia/en/5/5d/Genshin_Impact_logo.svg',
-    description: '提瓦特大陸的冒險指南與官方前瞻。',
-  },
-  {
-    name: '科技新報 TechNews',
-    category: '科技',
-    url: 'https://technews.tw/feed/',
-    logo_url: 'https://i.imgur.com/Uo2w9XG.png',
-    description: '最新的科技、Web 與軟體產業資訊分享。',
-  },
-  {
-    name: 'Steam 綜合遊戲情報',
-    category: '遊戲',
-    url: 'https://news.google.com/rss/search?q=Steam+Games&hl=zh-TW&gl=TW&ceid=TW:zh-Hant',
-    logo_url: 'https://upload.wikimedia.org/wikipedia/commons/8/83/Steam_icon_logo.svg',
-    description: 'Steam 平台特賣、新品與官方資訊。',
-  },
-  {
-    name: '台灣美食地圖',
-    category: '美食',
-    url: 'https://news.google.com/rss/search?q=台灣美食+OR+米其林&hl=zh-TW&gl=TW&ceid=TW:zh-Hant',
-    logo_url: 'https://cdn-icons-png.flaticon.com/512/3274/3274099.png',
-    description: '吃貨必看！全台最新美食餐廳與優惠情報。',
-  }
-];
+import { OFFICIAL_FEEDS } from '@/lib/official_communities';
 
 export async function GET(request: Request) {
   try {
